70d442ed78f203e42f8dd64dd935ec1cd98f284b
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-06-03 21:33:23 +0700

    Update

--- test.php
+++ test.php
@@ -1,9 +1,9 @@
 <?php
 
-$content = file_get_contents(__DIR__ . D . 'test');
+$content = file_get_contents(__DIR__ . D . 'test.txt');
 
 if ('POST' === $_SERVER['REQUEST_METHOD']) {
-    fire("x\\markdown__comment", [null, null, null, null], (object) []);
+    x\markdown__comment(null, null, null, null);
     echo '<pre style="background:#ccc;border:1px solid rgba(0,0,0,.25);color:#000;font:normal normal 100%/1.25 monospace;padding:.5em .75em;white-space:pre-wrap;word-wrap:break-word;">' . htmlspecialchars($content) . '</pre>';
     echo '<pre style="background:#cfc;border:1px solid rgba(0,0,0,.25);color:#000;font:normal normal 100%/1.25 monospace;padding:.5em .75em;white-space:pre-wrap;word-wrap:break-word;">' . htmlspecialchars($_POST['comment']['content'] ?? "") . '</pre>';
     exit;
